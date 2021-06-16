f9da71b3926f2ec93bdcaf0242339361ade1a3de
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-06-15 21:56:56 -0300

    first commit

---
+++ src/test/java/br/unicamp/mc851/evisita/oapi/srv/gateway/OapiSrvGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package br.unicamp.mc851.evisita.oapi.srv.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class OapiSrvGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
